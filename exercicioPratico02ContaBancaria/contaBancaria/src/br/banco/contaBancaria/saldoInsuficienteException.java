5bbae973b15307a2096ef0cb57cf9fe19749eecf
FelipeAdriano <adrianofelipe2004@gmail.com>
2024-07-04 15:41:27 -0300

    Subindo atividade pratica 2 - conta bancaria 03/07/2024

---
+++ exercicioPratico02ContaBancaria/contaBancaria/src/br/banco/contaBancaria/saldoInsuficienteException.java
@@ -0,0 +1,10 @@
+package br.banco.contaBancaria;
+
+public class saldoInsuficienteException extends Exception {
+	
+	public saldoInsuficienteException(String mensagem) {
+
+		super(mensagem);
+	}
+
+}
